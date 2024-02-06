56dd9505e99abed000f4c0d53a883d8f6bf6253c
Force143 <77406489+Force143@users.noreply.github.com>
2024-02-06 15:08:17 +0000

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -130,7 +130,7 @@ GameManager.prototype.move = function (direction) {
           
 	        var merged = new Tile(positions.next, tile.value * 1);
           if(next && next.value == 10) merged.value = tile.value * 1;
-          if(next && tile.value == 10) merged.value = next.value * 1;
+          if(next && tile.value == 10) merged.value = next.value * 10;
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
